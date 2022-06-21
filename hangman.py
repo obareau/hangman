7c7124b1d9b02fd5d35203f3b7a26dacb521d69d
Olivier BAREAU <olivier.bareau@gmail.com>
2022-06-21 14:24:48 +0200

    typo

--- hangman.py
+++ hangman.py
@@ -1,19 +1,48 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# just a little hangman,game for educcattional puurpose only
+# just a little hangman,game for educcational puurpose only
 import random
 
 trial = 7
 letter_found ="" 
 display = ""
+level = 1
 
 # Todo ! several lists with increasing dificculties // several game levels
-word_tuple = ["blender", "linux", "mac", "windows" , "freebsd", "solaris", "amigaos", "osx", "manjaro",]
+
+word_list1 = ["mac", "osx", "dos","bsd","foo","csh","zsh"]
+word_list2 = ["free", "bytes", "alpha","kilo","root","bash"]
+word_list3 = ["blender", "photoshop", "gimp", "illustrator", "python","amigaos"]
+word_list4 = ["procrastinator", "perpendicular", "supercalifragistics","kaleidoscopes","computationally" ]
+
+
+print(">> Bienvenue dans le pendu <<")
+level = int(input("Choisissez votre difficulté : [1,2,3,4])"))
+
+word_tuple = word_list1 #default value
+
+if level > 0:
+    word_tuple = word_list1
+if level > 1:
+    word_tuple = word_list2
+if level > 2:
+    word_tuple = word_list3
+if level > 3:
+    word_tuple = word_list4
+    
+
+
 solution = random.choice(word_tuple) # we pick one word from the list above
+
 
 # We print as many "-" than letters in "solutionn"
 for l in solution:
     display= display + "_ "
+
+
+
+
+
 
 
 # Gamelogic
